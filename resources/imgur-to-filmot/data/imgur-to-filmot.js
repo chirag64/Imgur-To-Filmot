0266d82ffcfcdf89c6b02d561496c2ae65c6c889
Chirag Bhatia <chiroo64@gmail.com>
2017-03-08 23:33:56 +0530

    Fixes #3 - https links don't work on filmot, changing https to http

--- resources/imgur-to-filmot/data/imgur-to-filmot.js
+++ resources/imgur-to-filmot/data/imgur-to-filmot.js
@@ -2,11 +2,15 @@ var links = document.getElementsByTagName("a");
 for (var i = 0; i < links.length; i++) {
 if (links[i].href.indexOf("imgur.com") != -1) {
     if (!!links[i].dataset.hrefUrl) {
+      links[i].dataset.hrefUrl = links[i].dataset.hrefUrl.replace("https://i.imgur.com","http://i.filmot.org");
+      links[i].dataset.hrefUrl = links[i].dataset.hrefUrl.replace("https://imgur.com","http://filmot.org");
       links[i].dataset.hrefUrl = links[i].dataset.hrefUrl.replace("imgur.com","filmot.org");
     }
     if (!!links[i].dataset.outboundUrl) {
       links[i].dataset.outboundUrl = '';
     }
+    links[i].href = links[i].href.replace("https://i.imgur.com","http://i.filmot.org");
+    links[i].href = links[i].href.replace("https://imgur.com","http://filmot.org");
     links[i].href = links[i].href.replace("imgur.com","filmot.org");
   }
 }

8ebb855ec48122970d387cb8ab41e7f893aa2925
Chirag Bhatia <chiroo64@gmail.com>
2017-03-08 23:30:07 +0530

    Fixed bug where reddit's outboundUrl tracker overrides our redirects back to imgur

--- resources/imgur-to-filmot/data/imgur-to-filmot.js
+++ resources/imgur-to-filmot/data/imgur-to-filmot.js
@@ -1,6 +1,12 @@
 var links = document.getElementsByTagName("a");
 for (var i = 0; i < links.length; i++) {
 if (links[i].href.indexOf("imgur.com") != -1) {
+    if (!!links[i].dataset.hrefUrl) {
+      links[i].dataset.hrefUrl = links[i].dataset.hrefUrl.replace("imgur.com","filmot.org");
+    }
+    if (!!links[i].dataset.outboundUrl) {
+      links[i].dataset.outboundUrl = '';
+    }
     links[i].href = links[i].href.replace("imgur.com","filmot.org");
   }
 }
